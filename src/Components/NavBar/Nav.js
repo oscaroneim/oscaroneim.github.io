b4f28da0d42e496b9cd3646b423308353f3fcbfe
Oscar Oneim <52040766+oscaroneim@users.noreply.github.com>
2024-04-12 12:22:38 +0100

    test3

---
+++ src/Components/NavBar/Nav.js
@@ -0,0 +1,16 @@
+export default function Nav() {
+  return (
+    <nav>
+      <div className="navBar">
+        <h1 className="myName">OSCAR ONEIM</h1>
+        <ul className="navItems">
+          <li>Projects</li>
+
+          <li>About</li>
+
+          <li>Contact</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
